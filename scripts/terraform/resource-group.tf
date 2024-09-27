0d887d1fd22bfb316fa55b65f2f7bcd9a0e7d927
Heta Patel <56798447+hetapatel638@users.noreply.github.com>
2024-09-28 01:45:49 +1000

    Update resource-group.tf

--- scripts/terraform/resource-group.tf
+++ scripts/terraform/resource-group.tf
@@ -2,6 +2,6 @@
 # Creates a resource group for sit722part5d in your Azure account.
 #
 resource "azurerm_resource_group" "sit722part5d" {
-  name     = var.app_name
-  location = var.location
+  name     = "sit722part5d"
+  location = "australiasoutheast"
 }
