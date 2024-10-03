148db7c4688ef67b4e6790bccc17f1cfb8441776
Heta Patel <56798447+hetapatel638@users.noreply.github.com>
2024-10-04 03:28:39 +1000

    Update kubernetes-cluster.tf

--- scripts/terraform/kubernetes-cluster.tf
+++ scripts/terraform/kubernetes-cluster.tf
@@ -28,7 +28,7 @@ resource "azurerm_kubernetes_cluster" "cluster" {
 
 resource "azurerm_role_assignment" "role_assignment" {
   principal_id                     = azurerm_kubernetes_cluster.cluster.kubelet_identity[0].object_id
-  role_definition_name             = "Contributor"
+  role_definition_name             = "AcrPull"
   scope                            = azurerm_container_registry.container_registry.id
   skip_service_principal_aad_check = true
 }
