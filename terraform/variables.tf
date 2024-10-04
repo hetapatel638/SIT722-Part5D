9debed8929876329ee1d1598062aeba19fe5205b
Heta Patel <56798447+hetapatel638@users.noreply.github.com>
2024-10-04 15:39:27 +1000

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -4,11 +4,11 @@ variable "location" {
 }
 
 variable "container_registry_name" {
-  default     = "sit722projectpart5l"
+  default     = "sit722projectpart5m"
 }
 
 variable "kubernetes_cluster_name" {
-  default     = "sit722projectpart5l"
+  default     = "sit722projectpart5m"
 }
 
 variable app_name {
